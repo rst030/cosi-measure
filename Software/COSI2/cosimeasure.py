af41c5db225f6e07ff752ada1458079a9275364b
git <git@github.com>
2024-08-27 12:58:40 +0200

    one insert map 240827

--- Software/COSI2/cosimeasure.py
+++ Software/COSI2/cosimeasure.py
@@ -303,8 +303,8 @@ class cosimeasure(object):
                     time.sleep(3)
                     ptidx = 0 # index of the point along the path
                     speed = 20 # [mm/s]
-                    t_offset = 1 # [s]
-                    t_meas = 1 # [s] measurement
+                    t_offset = 0.2 # [s]
+                    t_meas = 0.2 # [s] measurement
 
                     for pt in self.path.r: # follow the path
                         pt_prev = self.get_current_position(fakePosition=pt)
